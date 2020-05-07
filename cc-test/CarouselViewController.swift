c59cc38286cae7cf8267e4288ca34518338262bc
arkrayBergonia <fbergonia.arkray@gmail.com>
2020-05-07 16:36:24 +0800

    project complete

---
+++ cc-test/CarouselViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CarouselViewController.swift
+//  cc-test
+//
+//  Created by Francis Jemuel Bergonia on 5/7/20.
+//  Copyright © 2020 Xi Apps. All rights reserved.
+//
+
+import UIKit
+
+class CarouselViewController: UIViewController {
+
+    @IBOutlet weak var vwContainer: UIView!
+    @IBOutlet weak var btnNext: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func onButtonNextTap(_ sender: Any) {
+        // perform action 
+    }
+}
+
